86da6515ff5da76290e9ea1a309851e21f5f3652
sara <smorenoc19@educarex.es>
2025-11-19 18:27:49 +0100

    funcion tal

--- src/App.java
+++ src/App.java
@@ -2,4 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    public static void funcion() {
+        System.out.println("esto es una funcion");
+    }
+
+
 }
